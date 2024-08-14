57a44d4109d3c8da16965d3a8140435496ac68d9
Bryyyan <brayanpalacios93@gmail.com>
2024-08-14 19:06:46 +0200

    Cambios en config.py para forzar que el puerto sean enteros

--- Practica/app/config.py
+++ Practica/app/config.py
@@ -6,11 +6,14 @@ class Config:
     DB_HOST = os.getenv('DB_HOST')
     DB_PORT = os.getenv('DB_PORT')
     DB_NAME = os.getenv('DB_NAME')
-    print(f"DB_PORT: '{DB_PORT}'")  # Depuración
-    try:
-        DB_PORT = int(DB_PORT)
-    except ValueError:
-        raise ValueError(f"Invalid DB_PORT value: '{DB_PORT}'")
+
+    # Depuracion
+    print(f"DB_USER: {DB_USER}")
+    print(f"DB_PASS: {DB_PASS}")
+    print(f"DB_HOST: {DB_HOST}")
+    print(f"DB_PORT: {DB_PORT}")
+    print(f"DB_NAME: {DB_NAME}")
+
 
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False  
