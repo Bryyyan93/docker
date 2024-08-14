d8031723b7850367413708d822ffa65794036490
Bryyyan <brayanpalacios93@gmail.com>
2024-08-14 19:48:00 +0200

    Añadir un control para crear una DDBB solo si no existe

--- Practica/init.sql
+++ Practica/init.sql
@@ -1,3 +1,23 @@
-CREATE ROLE prueba1 WITH LOGIN PASSWORD 'example1';
-CREATE DATABASE notas;
-GRANT ALL PRIVILEGES ON DATABASE notas TO prueba1;
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT FROM pg_database
+        WHERE datname = 'notas'
+    ) THEN
+        CREATE DATABASE notas;
+    END IF;
+END
+$$;
+
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT FROM pg_roles
+        WHERE rolname = 'prueba1'
+    ) THEN
+        CREATE ROLE prueba1 WITH LOGIN PASSWORD 'example1';
+        GRANT ALL PRIVILEGES ON DATABASE notas TO prueba1;
+    END IF;
+END
+$$;
+
