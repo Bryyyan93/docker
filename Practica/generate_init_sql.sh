4ad2a19cad46abd4929aeeb4048f4cd4bc1b0456
Bryyyan <brayanpalacios93@gmail.com>
2024-08-15 12:07:33 +0200

    Cambios para que la DDBB se cree sola con las variables de entorno

---
+++ Practica/generate_init_sql.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Cargar las variables de entorno
+set -a
+./env_test1.env
+set +a
+
+# Generar el archivo SQL
+cat <<EOF > /docker-entrypoint-initdb.d/init.sql
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE ROLE IF NOT EXISTS $DB_USER WITH LOGIN PASSWORD '$DB_PASS';
+GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
+FLUSH PRIVILEGES;
+EOF
+
